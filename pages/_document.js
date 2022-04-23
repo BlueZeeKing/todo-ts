7b855aa5f12d2158c8b50a589a4c287456e6dd05
BlueZeeKing <52867955+BlueZeeKing@users.noreply.github.com>
2022-04-23 11:54:07 -0400

    fixed lang

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html land="en" className="bg-neutral-900">
+    <Html lang="en" className="bg-neutral-900">
       <Head>
         <meta name="description" content="Todo list built with typescript" />
         <link rel="icon" href="/favicon.ico" />
